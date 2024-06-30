085619e69fca97ded7453bea31894ed3fe7eb1ea
ajnobre <alvarojnobre@gmail.com>
2024-06-30 23:15:23 +0100

    Code refactoring and logging

--- tasks.py
+++ tasks.py
@@ -26,29 +26,22 @@ def handle_all_items():
         
         shutil.rmtree(directory_path)
     os.makedirs(directory_path)
-
     
     file_path = "output/output.xlsx"
 
     # Remove the file if it exists
     if os.path.exists(file_path):
         os.remove(file_path)
+    browser.configure(slowmo=100)
+    page = browser.page()
     
     for item in workitems.inputs:
         try:
             item = workitems.inputs.current
             logging.info(f"Processing item: {item}")
-            # payload = {
-            #     "topic": "summer",
-            #     "category": "Lifestyle",
-            #     "months": "24"
-            # }
-            # worktitem = workitems.outputs.create(payload)
-            # validate_data(worktitem)
-            # extract_news_data(worktitem)
             item = workitems.inputs.current
             validate_data(item)
-            extract_news_data(item)        
+            extract_news_data(item, page)        
             item.done()
             logging.info(f"Item processed successfully: {item}")
         except BusinessException as e:
@@ -57,18 +50,15 @@ def handle_all_items():
             item.fail(code=e.code, message=e.message)
         except ApplicationException as e:
             logging.error(f"Application error occurred: {e}")
+            page.screenshot(path="output/error_screenshot.png")
             # Handle application failure, e.g., by setting the work item to fail
             item.fail(code="APPLICATION_ERROR", message=str(e))
         except Exception as e:
             logging.error(f"Unexpected error occurred: {e}")
-            
+            page.screenshot(path="output/error_screenshot.png")       
             # Handle unexpected errors
             item.fail(code="UNEXPECTED_ERROR", message="An unexpected error occurred.")
-        # with item:  # This context manager automatically handles reserving and releasing the item
-        #     print("Received payload:", item.payload)
-        #     # Process the item here
-        #     # Example: Modify the payload or perform actions based on its content
-        #     # extract_news_data(item)
+
         
 def validate_data(workitem):
     """Validate the data in the work item payload."""
@@ -111,135 +101,32 @@ def validate_data(workitem):
             message="Invalid months value. The months must be a numeric value."
         )
     
-def extract_news_data(workitem):
+def extract_news_data(workitem,page ):
+    
+    # Extract the search phrase, category, and months from the work item payload
     search_phrase = workitem.payload.get("topic")
-    news_category = workitem.payload.get("category")
-    number_of_months = workitem.payload.get("months", 0)
+    category = workitem.payload.get("category")
+    months = workitem.payload.get("months", 0)
     
-    # Example function to fetch news data (to be implemented)
-    news_data = fetch_news(search_phrase, news_category, number_of_months)
+    open_browser(page)
     
-    # # Writing the fetched news data to the /output directory as JSON
-    # output_path = "/output/news_data.json"
-    # with open(output_path, "w") as file:
-    #     json.dump(news_data, file)
+    start_date = calculate_date_range(months)
     
-    # # Creating an output work item with the path to the saved news data
-    # workitems.outputs.create(payload={"news_data_path": output_path})
+    accept_cookies(page)
 
-def fetch_news(search_phrase, category, months):
-    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-    logging.info("Starting fetch_news function")
-    date_pattern = r"(\w+)\s(\d{1,2}),\s(\d{4})"
-    # Calculate the date range for the news articles
-    end_date = datetime.now()
-    start_date = end_date - relativedelta(months=int(months))
-    logging.info(f"Searching for news from {start_date} to {end_date}")
-    # Initialize the browser and open Reuters.com
-    browser.configure(slowmo=100)
-    page = browser.page()
-    page.goto("https://www.reuters.com/")
-    logging.info("Opened Reuters.com")
-    
-    # Introduce a human-like delay
-    
-    
-    # Accept cookies if necessary
-    try:
-        page.locator("#onetrust-accept-btn-handler").click()
-        logging.info("Accepted cookies")
-    except Exception:
-        page.screenshot(path="output/error_screenshot.png")
-        pass  # If the popup doesn't show up, ignore
-    
-    # Navigate to the search page and enter the search phrase
-    #Click on search icon
-    try:
-        page.click("//*[name()='path' and contains(@fill-rule,'evenodd')]")
-        logging.info("Clicked on search icon")
-    except Exception:
-        page.screenshot(path="output/error_screenshot.png")
-        pass  # If the popup doesn't show up, ignore
-    
-    
-    
-    input_locator = page.locator("//*[@data-testid='FormField:input']")
-    for char in search_phrase:
-        input_locator.type(char)
-        time.sleep(0.1)  # Adjust the delay as needed
-    page.press("//*[@data-testid='FormField:input']", "Enter")
+    navigate_and_search(page, search_phrase)
 
-    #     #Get paggination text
-    # pagination_locator = page.locator("//div[contains(@class, 'search-results__pagination__')]")
-    # pagination_text = pagination_locator.text_content()
-    # right_arrow_locator = page.locator("#SvgChevronRight")
+    select_category(page, category) 
     
-    #Select category
-    page.click(f"(//*[@data-testid='EnhancedSelect'])[1]")
-    page.click(f"(//*[@data-testid='EnhancedSelect'])[1]//span[text()='{category}']")
-    time.sleep(5)
-    search_next_page = True
-    list_items = []
-     # Initialize the Tables library
-    headers = ['title', 'date', 'description', 'picture filename', 'count of search phrases', 'contains money']
-    table = Tables().create_table(columns=headers)
+    extracted_news_table = process_search_results(page, start_date, search_phrase)
+
+    save_to_excel(extracted_news_table, filename="output.xlsx")
     
-    while search_next_page:
-        # Use a CSS selector with a partial match for the class name to find the <ul>
-        # and then select all <li> elements within it
-        #list_items_locator = page.locator("//ul[contains(@class, 'search-results__list__')]/li")
-        list_items_locator = page.query_selector_all("//ul[contains(@class, 'search-results__list__')]/li")
-        # Extracting the text from each list item and storing in a list
-
-        for item in list_items_locator: 
-            item.scroll_into_view_if_needed()
-            text = item.inner_text()
-            match = re.search(date_pattern, text)
-            if match:
-                title, extracted_date, count_search_phrase, contains_money_pattern = extract_info(text, search_phrase)
-                if extracted_date.date() < start_date.date():
-                    search_next_page = False
-                    break
-            # print("Selector "+item.query_selector("img").inner_html())
-            # page.wait_for_selector(item.query_selector("img"), timeout=timedelta(seconds=30))
-             
-            #img_src = fetch_image_src_with_retry(item)
-            
-            img_selector = item.wait_for_selector("//div[@data-testid='Image']//img")
-
-            # print("Image selector "+img_selector.as_element())  
-            img_src = img_selector.get_attribute("src")
-            
-
-            parsed_url = urlparse(img_src)
-            filename = "output/images/"+os.path.basename(parsed_url.path)
-            download_image(img_src, filename)
-            table.append_row([
-                title,
-                extracted_date.date().strftime("%d-%m-%Y") if extracted_date else '',
-                '',
-                filename,  
-                count_search_phrase,
-                contains_money_pattern
-            ])
-
-        
-        if not search_next_page:
-            break         
-        # Check if the right arrow button is disabled      
-        right_arrow_button_locator = page.locator("//*[@data-testid='SvgChevronRight']/ancestor::button[1]")
-
-        if right_arrow_button_locator.is_disabled():
-            search_next_page = False
-        else:
-            right_arrow_button_locator.click()
-        time.sleep(random.randint(5, 10))
-    # To print the table to verify
-    save_to_excel(table, filename="output.xlsx")
 
 
 # Function to extract title, date, and description
 def extract_info(text, search_phrase):
+    logging.info("Extracting information from text")
     date_match = re.search(r"(\w+)\s(\d{1,2}),\s(\d{4})", text)
     if date_match:
         # Extract the date components
@@ -250,31 +137,35 @@ def extract_info(text, search_phrase):
         #Extracted title
         parts = text.split('\n')
         title = parts[1] if len(parts) > 1 else ''
+        logging.info(f"Extracted title: {title}")
         
         count_search_phrase = title.lower().count(search_phrase.lower())
+        logging.info(f"Count of search phrase '{search_phrase}' in title: {count_search_phrase}")
         
          # Regular expression to match various money formats
         money_pattern = r"(\$[0-9,]+(\.[0-9]{1,2})?)|([0-9]+ (dollars|USD))"
         contains_money_pattern = bool(re.search(money_pattern, title))
+        logging.info(f"Contains money pattern: {contains_money_pattern}")
         return title, extracted_date, count_search_phrase, contains_money_pattern
+    else:
+        logging.error("Failed to extract date from text")
     return '', '', 0, False
 
 def fetch_image_src_with_retry(item, retries=3):
     attempt = 0
     while attempt < retries:
         try:
+            logging.info(f"Attempt {attempt+1}: Trying to fetch image source")
             # Use the Playwright API to interact with the web elements
             img_selector = item.wait_for_selector("//div[@data-testid='Image']//img")
 
-            # print("Image selector "+img_selector.as_element())  
             img_src = img_selector.get_attribute("src")
             if img_src:
+                logging.info("Successfully fetched image source")
                 return img_src
         except Exception as e:
+            logging.error(f"Attempt {attempt+1} failed with error: {e}")
             html_content = img_selector.evaluate("element => element.outerHTML")
-            print("Element "+html_content)
-            print(f"Attempt {attempt+1} failed: {str(e)}")
-            # browser.page().reload()  # Refresh the page or item
             time.sleep(1)  # Wait a bit before retrying
             attempt += 1
     
@@ -290,9 +181,12 @@ def download_image(url: str, filename: str ):
     """
 
     response = requests.get(url)
+    logging.info(f"Attempting to download content from {url}")
     response.raise_for_status()  # Ensure the request was successful
+
     with open(filename, 'wb') as file:
         file.write(response.content)
+        logging.info(f"Image download successful.")
         
 def save_to_excel(table, filename):
     """_summary_
@@ -301,6 +195,7 @@ def save_to_excel(table, filename):
         table (_type_): _description_
         filename (_type_): _description_
     """
+    logging.info("Saving extracted news data to Excel workbook 'output/output.xlsx")
      # Initialize the Excel file handler
     excel = Files()
     
@@ -313,4 +208,102 @@ def save_to_excel(table, filename):
     
     # Save and close the workbook
     excel.save_workbook()
+    logging.info("Closing the workbook")
     excel.close_workbook()
+
+    
+def accept_cookies(page):
+    try:
+        page.locator("#onetrust-accept-btn-handler").click()
+        logging.info("Accepted cookies")
+    except Exception:
+        page.screenshot(path="output/error_screenshot.png")
+        pass  # If the popup doesn't show up, ignore
+
+def navigate_and_search(page, search_phrase):
+     # Navigate to the search page and enter the search phrase
+    #Click on search icon
+    logging.info("Clicked search icon")
+    page.click("//*[name()='path' and contains(@fill-rule,'evenodd')]")
+    
+    logging.info(f"Searching for {search_phrase}")
+    input_locator = page.locator("//*[@data-testid='FormField:input']")
+    for char in search_phrase:
+        input_locator.type(char)
+        time.sleep(0.1)  # Adjust the delay as needed
+    page.press("//*[@data-testid='FormField:input']", "Enter")
+    
+def select_category(page, category):
+    #Select category
+    logging.info(f"Selecting category {category}")
+    page.click(f"(//*[@data-testid='EnhancedSelect'])[1]")
+    page.click(f"(//*[@data-testid='EnhancedSelect'])[1]//span[text()='{category}']")
+    time.sleep(5)
+
+def calculate_date_range(months):
+    
+    end_date = datetime.now()
+    start_date = end_date - relativedelta(months=int(months))
+    logging.info(f"Searching for news from {start_date} to {end_date}")
+    return start_date
+
+def process_search_results(page, start_date, search_phrase):
+    logging.info("Starting to extract and download news")
+    date_pattern = r"(\w+)\s(\d{1,2}),\s(\d{4})"
+    search_next_page = True
+    
+     # Initialize the table to store the extracted news data
+    headers = ['title', 'date', 'description', 'picture filename', 'count of search phrases', 'contains money']
+    extracted_news_table = Tables().create_table(columns=headers)
+    
+    while search_next_page:
+        logging.info("Processing next page of search results")
+
+        list_items_locator = page.query_selector_all("//ul[contains(@class, 'search-results__list__')]/li")
+
+        for item in list_items_locator: 
+            item.scroll_into_view_if_needed()
+            text = item.inner_text()
+            match = re.search(date_pattern, text)
+            if match:
+                title, extracted_date, count_search_phrase, contains_money_pattern = extract_info(text, search_phrase)
+                if extracted_date.date() < start_date.date():
+                    search_next_page = False
+                    logging.info("Reached news older than start date, stopping search")
+                    break
+
+            
+            img_selector = item.wait_for_selector("//div[@data-testid='Image']//img")
+
+            img_src = img_selector.get_attribute("src")
+            
+            parsed_url = urlparse(img_src)
+            filename = "output/images/"+os.path.basename(parsed_url.path)
+            logging.info(f"Downloading image from {img_src} to {filename}")
+            download_image(img_src, filename)
+            extracted_news_table.append_row([
+                title,
+                extracted_date.date().strftime("%d-%m-%Y") if extracted_date else '',
+                '',
+                filename,  
+                count_search_phrase,
+                contains_money_pattern
+            ])
+   
+        if not search_next_page:
+            break         
+        # Check if the right arrow button is disabled      
+        right_arrow_button_locator = page.locator("//*[@data-testid='SvgChevronRight']/ancestor::button[1]")
+
+        if right_arrow_button_locator.is_disabled():
+            logging.info("No more pages to process")
+            search_next_page = False
+        else:
+            right_arrow_button_locator.click()
+            logging.info("Moving to the next page")
+        time.sleep(random.randint(5, 10))
+    return extracted_news_table
+
+def open_browser(page):
+    page.goto("https://www.reuters.com/")
+    logging.info("Opened Reuters.com")
